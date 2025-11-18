444e0efa1e806b6b2deaddb096913cd34afc98c3
ibobal <119145325+ibobal@users.noreply.github.com>
2025-11-19 02:04:12 +0300

    Update Game.tsx

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,13 +1,15 @@
+import GuessInput from "@/components/GuessInput";
 import LetterCarousel from "@/components/LetterCarousel";
 import PreviewPlayer from "@/components/PreviewPlayer";
 import SongCard from "@/components/SongCard";
 
 export default function Game() {
   return (
-    <div>
+    <div className="flex flex-col justify-between flex-1 p-4">
       <LetterCarousel />
       <SongCard />
       <PreviewPlayer />
+      <GuessInput />
     </div>
   );
 }
