e06fec3081be90945d9d519ea15a1281b75d4a88
ibobal <119145325+ibobal@users.noreply.github.com>
2025-11-19 02:04:18 +0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,14 @@
+import { Music, ArrowLeftIcon, UserCircle } from "lucide-react";
+
 export default function Header() {
   return (
-    <header className="flex justify-center items-center py-2">
-      <h1 className="text-2xl font-bold">parolla-şarkı</h1>
+    <header className="flex justify-between items-center py-2 px-4 border-b">
+      <ArrowLeftIcon size={24} />
+      <div className="flex items-center gap-2">
+        <h1 className="text-2xl font-bold">parolla-şarkı</h1>
+        <Music size={24} />
+      </div>
+      <UserCircle size={24} />
     </header>
   );
 }
