14d480a78b9cb58b5c41cf375c194ce5cb7334a2
ibobal <119145325+ibobal@users.noreply.github.com>
2025-11-25 04:09:22 +0300

    Update Game.tsx

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,14 +1,20 @@
+import { useGameStore } from "@/store/game";
+import { useEffect } from "react";
 import GuessInput from "@/components/GuessInput";
 import LetterCarousel from "@/components/LetterCarousel";
-import PreviewPlayer from "@/components/PreviewPlayer";
 import SongCard from "@/components/SongCard";
 
 export default function Game() {
+  const loadSongs = useGameStore((state) => state.loadSongs);
+
+  useEffect(() => {
+    loadSongs();
+  }, [loadSongs]);
+
   return (
     <div className="flex flex-col justify-between flex-1 p-4">
       <LetterCarousel />
       <SongCard />
-      <PreviewPlayer />
       <GuessInput />
     </div>
   );
