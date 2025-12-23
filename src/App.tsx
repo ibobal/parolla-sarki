f1624a47237a50d4bdd6ce64a0ced294e3500643
ibobal <119145325+ibobal@users.noreply.github.com>
2025-12-23 22:40:47 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
+import { Outlet } from "react-router";
 import "./App.css";
 import Header from "./components/Header";
-import Game from "./pages/Game";
+import { ThemeProvider } from "./components/theme-provider";
 
 function App() {
   return (
-    <>
-      <main className="grid grid-cols-1 md:grid-cols-[1fr_minmax(480px,1.5fr)_1fr] min-h-screen md:divide-x divide-gray-200">
-        <section className="hidden md:flex items-center justify-center bg-gray-200 text-gray-500">AD</section>
-        <section className="flex flex-col min-h-screen bg-white">
-          <Header />
-          <Game />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <main className="grid grid-cols-1 md:grid-cols-[1fr_minmax(480px,1.5fr)_1fr] min-h-screen md:divide-x">
+        <section className="hidden md:flex items-center justify-center text-gray-500">
+          AD
         </section>
-        <section className="hidden md:flex items-center justify-center bg-gray-200 text-gray-500">AD</section>
+        <section className="flex flex-col">
+          <Header />
+          <Outlet />
+        </section>
+        <section className="hidden md:flex items-center justify-center text-gray-500">
+          AD
+        </section>
       </main>
-    </>
+    </ThemeProvider>
   );
 }
 
