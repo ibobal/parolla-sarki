aeb6d3798728da64500339e138d6e3fb1587fcdb
ibobal <119145325+ibobal@users.noreply.github.com>
2025-12-23 22:40:53 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import "./index.css";
+import App from "./App.tsx";
+import Home from "./pages/Home.tsx";
+import Game from "./pages/Game.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+    children: [
+      { index: true, Component: Home },
+      { path: "game", Component: Game },
+    ],
+  },
+]);
+
+
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />
+);
