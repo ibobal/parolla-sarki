d20af3edfb721d8f92d8e5aff671ba5815f98816
ibobal <119145325+ibobal@users.noreply.github.com>
2025-12-23 22:39:12 +0300

    Update Game.tsx

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -3,16 +3,22 @@ import { useEffect } from "react";
 import GuessInput from "@/components/GuessInput";
 import LetterCarousel from "@/components/LetterCarousel";
 import SongCard from "@/components/SongCard";
+import { Spinner } from "@/components/ui/spinner";
 
 export default function Game() {
   const startGame = useGameStore((state) => state.startGame);
-  
+  const isLoading = useGameStore((state) => state.isLoading);
+
   useEffect(() => {
     startGame();
   }, [startGame]);
 
-  return (
-    <div className="flex flex-col justify-between flex-1 p-4">
+  return isLoading ? (
+    <div className="flex flex-1 items-center justify-center">
+      <Spinner className="size-8"/>
+    </div>
+  ) : (
+    <div className="flex flex-col flex-1 justify-between p-4">
       <LetterCarousel />
       <SongCard />
       <GuessInput />
