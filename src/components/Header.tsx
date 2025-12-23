58730003738ce339b0bdc0ad40949b510c610fcd
ibobal <119145325+ibobal@users.noreply.github.com>
2025-12-23 22:40:40 +0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,35 @@
-import { Music, ArrowLeftIcon, UserCircle } from "lucide-react";
+import { Music, ArrowLeft, UserCircle, Globe } from "lucide-react";
+import { Link, useLocation } from "react-router";
+import { Button } from "./ui/button";
+import { ModeToggle } from "./mode-toggle";
 
 export default function Header() {
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
+
   return (
     <header className="flex justify-between items-center py-2 px-4 border-b">
-      <ArrowLeftIcon size={24} />
+      {isHomePage ? (
+        <Button variant="ghost" className="cursor-pointer">
+          <Globe className="scale-150" />
+        </Button>
+      ) : (
+        <Button variant="ghost" asChild>
+          <Link to="/">
+            <ArrowLeft className="scale-150" />
+          </Link>
+        </Button>
+      )}
       <div className="flex items-center gap-2">
         <h1 className="text-2xl font-bold">parolla-şarkı</h1>
-        <Music size={24} />
+        <Music />
       </div>
-      <UserCircle size={24} />
+      <div>
+        <ModeToggle />
+        <Button variant="ghost" className="cursor-pointer">
+          <UserCircle className="scale-150" />
+        </Button>
+      </div>
     </header>
   );
 }
