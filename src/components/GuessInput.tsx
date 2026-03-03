64ea14b5c8d5a5cb3e216404e6d82ba3f4b53cf6
ibobal <antrix06@gmail.com>
2026-03-03 04:40:40 +0300

    Update GuessInput.tsx

--- src/components/GuessInput.tsx
+++ src/components/GuessInput.tsx
@@ -2,13 +2,21 @@ import { ChevronRight } from "lucide-react";
 import { useState, useEffect } from "react";
 import { useGameStore } from "@/store/gameStore";
 import { Input } from "./ui/input";
+import { toast } from "sonner";
+import { normalizeText } from "@/lib/utils";
+import { Button } from "./ui/button";
 
 export default function GuessInput() {
   const [guess, setGuess] = useState("");
   const checkAnswer = useGameStore((state) => state.submitAnswer);
   const passTurn = useGameStore((state) => state.passTurn);
+  const finishGame = useGameStore((state) => state.finishGame);
+  const currentSong = useGameStore((state) => state.getCurrentSong());
+  const gameStatus = useGameStore((state) => state.gameStatus);
 
   useEffect(() => {
+    if (gameStatus !== "playing") return;
+
     const handleKeyDown = (event: KeyboardEvent) => {
       if (event.key === "Tab") {
         event.preventDefault();
@@ -17,8 +25,11 @@ export default function GuessInput() {
     };
 
     window.addEventListener("keydown", handleKeyDown);
-    return () => window.removeEventListener("keydown", handleKeyDown);
-  }, [passTurn]);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [gameStatus, passTurn]);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setGuess(event.target.value);
@@ -26,11 +37,27 @@ export default function GuessInput() {
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
     if (guess === "pas") {
       passTurn();
       setGuess("");
       return;
+    } else if (guess === "bitir") {
+      finishGame();
+      setGuess("");
     }
+
+    if (currentSong) {
+      const normalizedAnswer = normalizeText(guess);
+      const songLetterNormalized = normalizeText(currentSong.letter);
+
+      if (!normalizedAnswer.startsWith(songLetterNormalized) && normalizedAnswer !== "pas" && normalizedAnswer !== "bitir") {
+        toast.warning(`Cevap "${currentSong.letter}" harfi ile başlamalı`);
+        setGuess("");
+        return;
+      }
+    }
+
     checkAnswer(guess);
     setGuess("");
   };
@@ -47,20 +74,26 @@ export default function GuessInput() {
         className="w-full px-4 py-6 pr-20 rounded-lg"
         value={guess}
         onChange={handleChange}
+        autoComplete="off"
+        disabled={gameStatus !== "playing"}
       />
       {guess.length === 0 ? (
-        <button
+        <Button
           onClick={passTurn}
-          className="absolute right-1 flex items-center gap-1 bg-warning px-3 hover:bg-amber-400 py-2 rounded-sm text-md font-medium text-white"
+          className="absolute right-1.5 flex items-center bg-warning hover:bg-amber-400 rounded-sm text-md font-medium text-white"
+          disabled={gameStatus !== "playing"}
         >
           <ChevronRight className="w-6 h-6" />
           <span>PAS</span>
-        </button>
+        </Button>
       ) : (
-        <button className="absolute right-1 flex items-center gap-1 bg-error hover:bg-red-400 px-3 py-2 rounded-sm text-md font-medium text-white">
+        <Button
+          className="absolute right-1.5 flex items-center bg-error hover:bg-red-400 rounded-sm text-md font-medium text-white"
+          disabled={gameStatus !== "playing"}
+        >
           <ChevronRight className="w-6 h-6" />
           <span>GÖNDER</span>
-        </button>
+        </Button>
       )}
     </form>
   );
